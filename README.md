46affe6891d3728f4609b0a7249128315566dd90
David Bennett <david.a.bennett7@gmail.com>
2023-08-26 10:13:09 -0400

    readme updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# ESP32 Digital Mom Sign
+
+A custom digital photo frame that I built my mom for mother's day. It uses [AWS infrastructure](https://github.com/DaveBben/digital_mom_sign_cloud_infrastructure/tree/main) to display photos from an S3 bucket.
+
+![Esp32 Photo Frame](demo_image.jpeg)
--- src/main.ino
+++ src/main.ino
@@ -1,23 +1,16 @@
-/*
- An example showing rainbow colours on a 2.2" TFT LCD screen
- and to show basic examples of font use.
-
- This sketch uses the GLCD, 2, 4, 6 fonts only.
-
- Make sure all the display driver and pin connections are correct by
- editing the User_Setup.h file in the TFT_eSPI library folder.
-
- #########################################################################
- ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
- #########################################################################
- */
-
-#include <TFT_eSPI.h> // Hardware-specific library
+#include "JPEGDEC.h"
+#include <TFT_eSPI.h>
 #include <SPI.h>
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
 
-TFT_eSPI tft = TFT_eSPI();                   // Invoke custom library with default width and height
+TFT_eSPI tft = TFT_eSPI();
 
-//TFT_eSPI tft = TFT_eSPI(240, 320);       // Could invoke custom library declaring width and height
+// TFT_eSPI tft = TFT_eSPI(240, 320);       // Could invoke custom library declaring width and height
 
 unsigned long targetTime = 0;
 byte red = 31;
@@ -26,130 +19,222 @@ byte blue = 0;
 byte state = 0;
 unsigned int colour = red << 11; // Colour order is RGB 5+6+5 bits each
 
-void setup(void) {
-  Serial.begin(9600);
+// 50kb
+const uint16_t FRAME_BUFFER_SIZE = 50000;
+uint8_t *video_frame_buffer;
+
+// The MQTT topics that this device should publish/subscribe
+#define AWS_IOT_PUBLISH_TOPIC "esp32_photo_frame/metrics"
+#define AWS_IOT_SUBSCRIBE_TOPIC_1 "new_image_available"
+#define AWS_IOT_SUBSCRIBE_TOPIC_2 "command/device/esp32_photo_frame"
+
+WiFiClientSecure net = WiFiClientSecure();
+WiFiClientSecure net2 = WiFiClientSecure();
+HTTPClient https;
+MQTTClient client = MQTTClient(512);
+JPEGDEC jpeg;
+
+void connect_wifi()
+{
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  Serial.println("Connecting to Wi-Fi");
+  uint8_t counter = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+    if (counter > 30)
+    {
+      esp_restart();
+    }
+    counter += 1;
+  }
+}
+
+void connectAWS()
+{
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.begin(AWS_IOT_ENDPOINT, 8883, net);
+
+  // Create a message handler
+  client.onMessage(messageHandler);
+
+  Serial.print("Connecting to AWS IOT");
+  uint8_t counter = 0;
+  if (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    Serial.printf("clientMQTT.lastError = %d\n", client.lastError());
+    Serial.printf("clientMQTT.returnCode = %d\n", client.returnCode());
+    delay(5000);
+    esp_restart();
+  }
+
+  // Subscribe to a topic
+  if (client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC_1))
+  {
+    Serial.println("Successfully subscribed to Topic 1");
+  }
+  if (client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC_2))
+  {
+    Serial.println("Successfully subscribed to Topic 2");
+  }
+
+  Serial.println("AWS IoT Connected!");
+  get_image();
+}
+
+void publish_metrics()
+{
+  StaticJsonDocument<200> doc;
+  doc["signal_strength"] = WiFi.RSSI();
+  doc["ip_address"] = WiFi.localIP().toString();
+  doc["temperature"] = temperatureRead();
+  doc["heap_memory_free_bytes"] = esp_get_free_heap_size();
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+
+void messageHandler(String &topic, String &payload)
+{
+  // Serial.println("incoming: " + topic + " - " + payload);
+  if (topic == AWS_IOT_SUBSCRIBE_TOPIC_1)
+  {
+    get_image();
+  }
+  else if (topic == AWS_IOT_SUBSCRIBE_TOPIC_2)
+  {
+    if (payload == "OFF")
+    {
+      // Serial.println("Turning off display");
+      tft.writecommand(0x10);
+    }
+    if (payload == "ON")
+    {
+      // Serial.println("Turning on display");
+      tft.init();
+    }
+    if (payload == "RESTART")
+    {
+      // Serial.println("Restarting system");
+      esp_restart();
+    }
+    if (payload == "METRICS")
+    {
+      publish_metrics();
+    }
+  }
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
   tft.init();
   tft.setRotation(2);
   tft.fillScreen(TFT_BLACK);
-
-  targetTime = millis() + 1000;
+  video_frame_buffer = (uint8_t *)malloc(sizeof(uint8_t) * FRAME_BUFFER_SIZE);
+  connect_wifi();
+  connectAWS();
 }
 
-void loop() {
-
-  if (targetTime < millis()) {
-    targetTime = millis() + 10000;
-
-    rainbow_fill(); // Fill the screen with rainbow colours
-
-    // The standard AdaFruit font still works as before
-    tft.setTextColor(TFT_BLACK); // Background is not defined so it is transparent
- 
-    tft.setCursor (60, 5);
-    tft.setTextFont(0);        // Select font 0 which is the Adafruit font
-    tft.print("Original Adafruit font!");
-
-    //tft.drawString("Original Adafruit font!",60,5,1); 
-
-    // The new larger fonts do not need to use the .setCursor call, coords are embedded
-    tft.setTextColor(TFT_BLACK); // Do not plot the background colour
-    // Overlay the black text on top of the rainbow plot (the advantage of not drawing the backgorund colour!)
-    tft.drawCentreString("Font size 2", 120, 14, 2); // Draw text centre at position 120, 14 using font 2
-    tft.drawCentreString("Font size 4", 120, 30, 4); // Draw text centre at position 120, 30 using font 4
-    tft.drawCentreString("12.34", 120, 54, 6);       // Draw text centre at position 120, 54 using font 6
-
-    tft.drawCentreString("12.34 is in font size 6", 120, 92, 2); // Draw text centre at position 120, 92 using font 2
-    // Note the x position is the top of the font!
-
-    // draw a floating point number
-    float pi = 3.14159; // Value to print
-    int precision = 3;  // Number of digits after decimal point
-    int xpos = 90;      // x position
-    int ypos = 110;     // y position
-    int font = 2;       // font number 2
-    xpos += tft.drawFloat(pi, precision, xpos, ypos, font); // Draw rounded number and return new xpos delta for next print position
-    tft.drawString(" is pi", xpos, ypos, font);             // Continue printing from new x position
-
-    tft.setTextSize(1);           // We are using a size multiplier of 1
-
-    tft.setTextColor(TFT_BLACK);  // Set text colour to black, no background (so transparent)
-
-    tft.setCursor(36, 150, 4);    // Set cursor to x = 36, y = 150 and use font 4
-    tft.println("Transparent...");  // As we use println, the cursor moves to the next line
-
-    tft.setCursor(30, 175);    // Set cursor to x = 30, y = 175
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);  // Set text colour to white and background to black
-    tft.println("White on black");
-
-    tft.setTextFont(4);        // Select font 4 without moving cursor
-    tft.setCursor(50, 210);    // Set cursor to x = 50, y = 210 without changing the font
-    tft.setTextColor(TFT_WHITE);
-    // By using #TFT print we can use all the formatting features like printing HEX
-    tft.print(57005, HEX);    // Cursor does no move to next line
-    tft.println(48879, HEX);  // print and move cursor to next line
-
-    tft.setTextColor(TFT_GREEN, TFT_BLACK); // This time we will use green text on a black background
-    tft.setTextFont(2); // Select font 2
-    //Text will wrap to the next line if needed, by luck it breaks the lines at spaces!
-    tft.println(" Ode to a Small Lump of Green Putty I Found in My Armpit One Midsummer Morning ");
-  }
-}
-
-// Fill screen with a rainbow pattern
-void rainbow_fill()
+int JPEGDraw(JPEGDRAW *pDraw)
 {
-  // The colours and state are not initialised so the start colour changes each time the funtion is called
-  
-  for (int i = 319; i > 0; i--) {
-    // Draw a vertical line 1 pixel wide in the selected colour
-    tft.drawFastHLine(0, i, tft.width(), colour); // in this example tft.width() returns the pixel width of the display
-    // This is a "state machine" that ramps up/down the colour brightnesses in sequence
-    switch (state) {
-      case 0:
-        green ++;
-        if (green == 64) {
-          green = 63;
-          state = 1;
+  int iCount;
+  iCount = pDraw->iWidth * pDraw->iHeight; // number of pixels to draw in this call
+  // Serial.printf("Drawing %d pixels\n", iCount);
+  tft.pushImage(pDraw->x, pDraw->y, pDraw->iWidth, pDraw->iHeight, pDraw->pPixels);
+  return 1;
+}
+
+void get_image()
+{
+  // Serial.println("Grabbing image");
+  net2.setCACert(AWS_CERT_CA);
+  https.begin(net2, HOST_URL);
+  https.addHeader("Accept", "image/jpeg");
+  https.addHeader("x-api-token", API_SECRET);
+  uint16_t timeout = 8000;
+  https.setTimeout(timeout);
+  int response = https.GET();
+  // Serial.println(response);
+  if (response > 0)
+  {
+    if (response == HTTP_CODE_OK)
+    {
+      int original_length = https.getSize();
+      if (original_length < FRAME_BUFFER_SIZE)
+      {
+        int len = original_length;
+        WiFiClient *stream = https.getStreamPtr();
+        // read all data from server
+        while (https.connected() && len > 0)
+        {
+          // get available data size
+          size_t size = stream->available();
+          // Serial.println(size);
+          if (size)
+          {
+            int c = stream->readBytes(video_frame_buffer, len);
+            if (len > 0)
+            {
+              len -= c;
+            }
+          }
+          delay(1);
         }
-        break;
-      case 1:
-        red--;
-        if (red == 255) {
-          red = 0;
-          state = 2;
+        // display image
+        tft.startWrite();
+        if (jpeg.openRAM(video_frame_buffer, original_length, JPEGDraw))
+        {
+          jpeg.setPixelType(RGB565_BIG_ENDIAN);
+          if (jpeg.decode(0, 0, 0))
+          {
+            tft.endWrite();
+          }
+          else
+          {
+            Serial.println("Decode error");
+          }
+          jpeg.close();
         }
-        break;
-      case 2:
-        blue ++;
-        if (blue == 32) {
-          blue = 31;
-          state = 3;
+        else
+        {
+          Serial.println("Failed to open JPEG");
         }
-        break;
-      case 3:
-        green --;
-        if (green == 255) {
-          green = 0;
-          state = 4;
-        }
-        break;
-      case 4:
-        red ++;
-        if (red == 32) {
-          red = 31;
-          state = 5;
-        }
-        break;
-      case 5:
-        blue --;
-        if (blue == 255) {
-          blue = 0;
-          state = 0;
-        }
-        break;
+      }
     }
-    colour = red << 11 | green << 5 | blue;
+    else
+    {
+      Serial.println("response failed");
+    }
+    // Serial.println("Done");
   }
 }
 
-
+void loop()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    if (client.connected())
+    {
+      client.loop();
+    }
+    else
+    {
+      connectAWS();
+    }
+  }
+  else
+  {
+    connect_wifi();
+  }
+}
